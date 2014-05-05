82c5032010e4f3ff7478348bb4e9dcf929f88e0e
vkavasis <basiliskavasis@gmail.com>
2014-05-05 11:19:41 +0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DissertionRb::Application.config.secret_key_base = 'dc4cd13a3e8034ae911b118139ffcee5b06da1646d21606700d4e0497cc2c38c18843173bd5c7711139ba645199006411f93f1af857d2fb3c5c49798aa629938'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DissertionRb::Application.config.session_store :cookie_store, key: '_dissertion_rb_session'
