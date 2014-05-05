3b022a5dff307f544cf6732f24704f2f5d9eaa49
vkavasis <basiliskavasis@gmail.com>
2014-05-05 13:34:57 +0300

    Make sign in with username and email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,4 +29,17 @@ DissertionRb::Application.configure do
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+      :address => "smtp.gmail.com",
+      :port => 587,
+      :domain => "gmail.com",
+      :user_name => "basiliskavasis@gmail.com",
+      :password => "33101990",
+      :authentication => 'plain',
+      :enable_starttls_auto => true
+  }
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 DissertionRb::Application.routes.draw do
+  get "registrations/update"
   get "home/index"
-  devise_for :users
+  devise_for :users, :controllers => {:registrations => "registrations"}
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
